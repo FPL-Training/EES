7ff48e66722648965e0393be4c804faa3669159f
Admin <Admin@BLACKHILL.fios-router.home>
2017-03-18 10:04:48 -0400

    Add license disclaimer/header

--- src/main/java/com/fastprac/sees/driver/RunEES1.java
+++ src/main/java/com/fastprac/sees/driver/RunEES1.java
@@ -1,5 +1,13 @@
-/**
- * 
+/*
+ * *********************************************************************************
+ * Copyright (c) Fast & Practical Learning LLC (F&PL), 2017
+ *
+ * This unpublished material is proprietary to Fast & Practical Learning, LLC (F&PL).
+ * All rights reserved. The methods and techniques described herein are considered 
+ * trade secrets and/or confidential. Reproduction or distribution, in whole or in 
+ * part, is forbidden except by express written permission of Fast & Practical 
+ * Learning, LLC (F&PL).
+ ***********************************************************************************
  */
 package com.fastprac.sees.driver;
 
